54838281c90e6015c54f3c488d9614c14a447f18
Yukichi <yukichi0306@hotmail.co.jp>
2018-09-07 16:09:38 +0900

    直接PNG書き出し

---
+++ DirectPngSave.jsx
@@ -0,0 +1,31 @@
+﻿/* 
+==============================================================================================
+DirectPngSave@ToyBox
+Last Update:2018/09/07
+https://github.com/yukichi0306/
+==============================================================================================
+*/
+var docObj = activeDocument;
+var layList = [];
+var pathEgg;
+var filePath;
+
+for (i = 0; i <docObj.layers.length; i++)
+{
+    layList[i] = docObj.layers[i].name;
+
+if(layList[i].indexOf('++' ) !== -1)
+{
+    pathEgg = layList[i];
+    }
+}
+filePath = pathEgg.replace('++', '');
+
+//PngSave========================================================================
+pngFile = new File(filePath);//パス指定とファイル名
+pngOpt = new PNGSaveOptions();
+pngOpt.interlaced = false;
+activeDocument.saveAs(pngFile, pngOpt, true, Extension.LOWERCASE);  
+//===============================================================================
+
+alert('Finish');
