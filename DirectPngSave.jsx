158af92e18745210e4e35046cc0d3c6ff16f44d6
Yukichi <yukichi0306@hotmail.co.jp>
2018-09-11 22:22:46 +0900

    レイヤーの検索処理変更と合致数の違いによる処理を追加

--- DirectPngSave.jsx
+++ DirectPngSave.jsx
@@ -1,30 +1,50 @@
 ﻿/* 
 ==============================================================================================
 DirectPngSave@ToyBox
-Last Update:2018/09/07
+Last Update:2018/09/11
 https://github.com/yukichi0306/
 
 必要と想定される例外処理------------------------------------------
-・pathEggが無い場合に警告
-・pathEggに該当するものが２以上ある場合、警告
+（実装済）pathEggが無い場合に警告（18/09/11）
+（実装済）pathEggに該当するものが２以上ある場合、警告(18/09/11)
+・layListかpathEggのどちらかがいらないんじゃ…？
 ・指定先のフォルダが無い場合に警告
 ==============================================================================================
 */
 var docObj = activeDocument;
 var layList = [];
-var pathEgg;
+var pathEgg = [];   //保存先候補
 var filePath;
 
 for (i = 0; i <docObj.layers.length; i++)
 {
     layList[i] = docObj.layers[i].name;
-
-if(layList[i].indexOf('++' ) !== -1)
-{
-    pathEgg = layList[i];
+    if(layList[i].indexOf('++') !== -1)
+    {
+        for(j = 0;j <= i; j++)
+        {
+        pathEgg[j] = layList[i];
+        }
     }
 }
-filePath = pathEgg.replace('++', '');
+if(pathEgg.length == 0)
+{
+    alert("保存先を示したレイヤーが見つかりません。");
+    }
+else if(pathEgg.length == 1)
+{
+    DirectPngSave ();
+    }
+else if(pathEgg.length > 1)
+{
+    alert("保存先を示したレイヤーが２つ以上あります。");
+    }
+
+
+//画像を保存する====================================================================
+function DirectPngSave()
+{
+filePath = pathEgg[0].replace('++', '');
 
 //PngSave========================================================================
 pngFile = new File(filePath);//パス指定とファイル名
@@ -34,3 +54,4 @@ activeDocument.saveAs(pngFile, pngOpt, true, Extension.LOWERCASE);
 //===============================================================================
 
 alert('Finish');
+}
